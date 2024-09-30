a572db1458162250a0a53faf91048fa9724f8802
Noah Fast <nfastmr@gmail.com>
2024-09-29 23:03:20 -0400

    Added ExponentialOddonacci.class to .gitignore Cleaned up ExponentialOddonacci.java

--- src/ExponentialOddonacci.java
+++ src/ExponentialOddonacci.java
@@ -1,31 +1,23 @@
 // Oddonacci Algorithm:
 // Input: Nonnegative integer k.
 // Output: the kth Oddonnacci number O(k).
-// 
 // If k <= 3, return 1
 // else, return binOddonacci(k - 1) + binOddonacci(k - 2) + binOddonacci(k - 3).
 
-//import java.util.Scanner;
-
-public class BinaryOddonacci {
-
+public class ExponentialOddonacci {
 	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		int result = binOddonacci(sc.nextInt());
-//		sc.close();
-//		System.out.println("Result = " + result);
-		for(int i = 5; i < 200; i = i + 5) {
+		for(int i=5; i<200; i=i+5) {
 			long startTime = System.nanoTime();
-			binOddonacci(i);
+			int result = expOddonacci(i);
 			long endTime = System.nanoTime();
-			System.out.println("Time for Oddonacci(" + i + ")" + " = " + (endTime - startTime)/1000000000.0 + " seconds");
+			System.out.println(String.format("%-13s","Oddonacci("+i+")")+" = "+String.format("%-11d",result)+" | Duration: "+ String.format("%11.7f",((endTime-startTime)/1000000000f))+" seconds");
 		}
 	}
-	public static int binOddonacci(int k) {
-		if(k <= 3) {
+	public static int expOddonacci(int k) {
+		if(k<=3) {
 			return 1;
 		} else {
-			return binOddonacci(k - 1) + binOddonacci(k - 2) + binOddonacci(k - 3);
+			return expOddonacci(k-1)+expOddonacci(k-2)+expOddonacci(k-3);
 		}
 	}
 }
